5180c2aa64042acfff113fbf1673926cb27b1a50
ferdakesik <ferdakesik@gmail.com>
2023-05-06 13:57:17 -0400

    fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,42 @@
-$(function(){
-    $(".carousel").carousel({interval:2000});
-    $("#carouselButton").click(function(){
-        if($("#carouselButton").children("i").hasClass("fa-pause")){
-            $(".carousel").carousel("pause");
-            $("#carouselButton").children("i").removeClass("fa-pause");
-            $("#carouselButton").children("i").addClass("fa-play");
-        }else{
-            $(".carousel").carousel("cycle");
-            $("#carouselButton").children("i").removeClass("fa-play");
-            $("#carouselButton").children("i").addClass("fa-pause");
-        }
+
+    $(function() {
+
+
+      $('#reserveButton').on("click", function() {
+        $('#reserveModal').modal('show');
+      });
+    
+      // Activate Login modal on link click
+      $('#loginButton').on("click", function() {
+        $('#loginModal').modal('show');
+      });
+
+
+
+        $(".carousel").carousel( { interval: 2000 } );
+        $("#carouselButton").on("click",function(){
+            if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+                $(".carousel").carousel("pause");
+                $("#carouselButton").children("i").removeClass("fa-pause");
+                $("#carouselButton").children("i").addClass("fa-play");
+            } else {
+                $(".carousel").carousel("cycle");
+                $("#carouselButton").children("i").removeClass("fa-play");
+                $("#carouselButton").children("i").addClass("fa-pause"); 
+            }
+
+        });
+      
     });
-});
+
+
+
+
+
+
+
+
+
+
+
+   
